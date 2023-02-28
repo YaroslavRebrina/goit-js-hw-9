936f58012768c0a3f7e340c224e2d787491792fe
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-02-28 17:57:06 +0200

    task 3 refactored

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,7 +5,6 @@ import 'notiflix/dist/notiflix-3.2.6.min.css';
 const form = document.querySelector('.form');
 
 let data = {};
-let delay = 0;
 
 const onInput = e => {
   data[e.target.name] = Number(e.target.value);
@@ -15,23 +14,23 @@ form.addEventListener('input', onInput);
 form.addEventListener('submit', onSubmit);
 
 function createPromise(position, delay) {
-  setTimeout(() => {
-    const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
         reject({ position, delay });
       }
-    });
-  }, delay);
+    }, delay);
+  });
 }
 
 function onSubmit(e) {
   e.preventDefault();
 
-  for (let i = 1; i <= data.amount; i += 1) {
-    createPromise(i, Number(data.delay) + Number(data.step) * i)
+  for (let i = 0; i <= data.amount; i += 1) {
+    createPromise(i + 1, Number(data.delay) + Number(data.step) * i)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
